05cbb6c55f4a26cb9b6c74792910c8d19b6f666d
liujianuo <78994438+liujianuo@users.noreply.github.com>
2023-04-11 16:03:43 -0700

    TemplateCreate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/template.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="TeleOpOne")
+public class Template extends OpMode {
+    public void init(){
+        
+    }
+
+    public void loop(){
+    
+    }
+
+    public void stop(){
+
+    }
+}
+
